9607113389a6078d01faa1a13ecfcb4b8c8e345c
BassamAMansour <bsbsaiman@gmail.com>
2017-10-14 22:27:34 +0200

    Initial commit

---
+++ app/src/main/java/com/csed2020/z_thesmartcar/MainActivity.java
@@ -0,0 +1,13 @@
+package com.csed2020.z_thesmartcar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
